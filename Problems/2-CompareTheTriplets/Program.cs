ba05565a3ace6e61f955f8edf9080ba24afd243c
serinkanosman <serinkanosman@gmail.com>
2025-03-19 23:21:21 +0300

    yeni soru eklendi

---
+++ Problems/2-CompareTheTriplets/Program.cs
@@ -0,0 +1,134 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+/*
+
+Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for three categories: problem clarity, originality, and difficulty.
+
+The rating for Alice's challenge is the triplet a = (a[0], a[1], a[2]), and the rating for Bob's challenge is the triplet b = (b[0], b[1], b[2]).
+
+The task is to calculate their comparison points by comparing each category:
+
+If a[i] > b[i], then Alice is awarded 1 point.
+If a[i] < b[i], then Bob is awarded 1 point.
+If a[i] = b[i], then neither person receives a point.
+Example
+
+a = [1, 2, 3]
+b = [3, 2, 1]
+
+For elements *0*, Bob is awarded a point because a[0] < b[0].
+For the equal elements a[1] and b[1], no points are earned.
+Finally, for elements 2, a[2] > b[2] so Alice receives a point.
+The return array is [1, 1] with Alice's score first and Bob's second.
+
+Function Description
+
+Complete the function compareTriplets with the following parameter(s):
+
+int a[3]: Alice's challenge rating
+int b[3]: Bob's challenge rating
+Returns
+
+int[2]: the first element is Alice's score and the second is Bob's score
+Input Format
+
+The first line contains 3 space-separated integers, a[0], a[1], and a[2], the respective values in triplet a.
+The second line contains 3 space-separated integers, b[0], b[1], and b[2], the respective values in triplet b.
+
+Constraints
+
+1 ≤ a[i] ≤ 100
+1 ≤ b[i] ≤ 100
+Sample Input 0
+
+5 6 7
+3 6 10
+Sample Output 0
+
+1 1
+Explanation 0
+
+In this example:
+
+-a = (a[0], a[1], a[2]) = (5,6,7)
+
+-b = (b[0], b[1], b[2]) = (3,6,10)
+
+Now, let's compare each individual score:
+a[0] > b[0] , so Alice receives  point.
+a[1] = b[1] , so nobody receives a point.
+a[2] < b[2] , so Bob receives  point.
+
+Alice's comparison score is , and Bob's comparison score is . Thus, we return the array .
+
+Sample Input 1
+17 28 30
+99 16 8
+Sample Output 1
+2 1
+Explanation 1
+
+Comparing the 0th elements, 17<99  so Bob receives a point.
+Comparing the 1st and 2nd elements, 28> 16 and 30 > 8  so Alice receives two points.
+The return array is [2,1].
+*/
+
+class Result
+{
+
+    /*
+     * Complete the 'compareTriplets' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER_ARRAY b
+     */
+
+    public static List<int> compareTriplets(List<int> a, List<int> b)
+    {
+        int aliceScore = 0;
+        int bobScore = 0;
+
+        for (int i = 0; i < 3; i++){
+
+        
+            if (a[i] > b[i])
+                aliceScore++;
+            else if (a[i] < b[i])
+                bobScore++;
+        }
+
+        return new List<int> { aliceScore, bobScore };
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+
+        List<int> result = Result.compareTriplets(a, b);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ Problems/3-SolveMeFirst/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+/*
+
+Complete the function  to compute the sum of two integers.
+
+Example
+
+
+Return .
+
+Function Description
+
+Complete the  function with the following parameters:
+
+: the first value
+: the second value
+Returns
+- : the sum of  and 
+
+Constraints
+
+
+Sample Input
+
+a = 2
+b = 3
+Sample Output
+
+5
+Explanation
+2+3 = 5
+
+*/
+namespace SolveMeFirst{
+public class Program{
+      
+            // Toplama işlemi yapan fonksiyon
+        public static int solveMeFirst(int a, int b) 
+        { 
+                return a + b; // İki sayıyı toplar
+        }
+        
+
+        static void Main(String[] args) 
+        {
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            int val2 = Convert.ToInt32(Console.ReadLine());
+            int sum = solveMeFirst(val1,val2);
+            Console.WriteLine(sum);
+        }
+    }
+}
